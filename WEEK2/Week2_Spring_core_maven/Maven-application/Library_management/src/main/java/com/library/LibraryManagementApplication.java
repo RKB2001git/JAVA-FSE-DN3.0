cadd9b51756ffa4164fedfe75d1bc267adbd49e5
Rajat Kumar Barik <iamrkb2001@gmail.com>
2024-09-01 16:47:55 +0530

    week2 assignment maven & springboot
    created one maven application & springboot application for library_management app

---
+++ WEEK2/Week2_Spring_core_maven/Maven-application/Library_management/src/main/java/com/library/LibraryManagementApplication.java
@@ -0,0 +1,19 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+        // Load Spring context from XML configuration file
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Retrieve the BookService bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Test the configuration by calling a method on the service
+        bookService.displayBooks();
+    }
+}
